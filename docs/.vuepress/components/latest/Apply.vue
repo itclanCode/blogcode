caea54c4230105664854afec3c331a0a6a91d207
itclancode <itclancode@163.com>
2021-11-13 22:06:51 +0800

    调整首页图片展示

--- docs/.vuepress/components/latest/Apply.vue
+++ docs/.vuepress/components/latest/Apply.vue
@@ -3,8 +3,8 @@
     <div class="wrap">
       <div v-for="item in applyData" class="item" :key="item.alt">
         <p class="list">
-          <a :href="item.href" target="_blank" :title="item.alt">
-            <img :width="item.width" :height="item.height" class="img-pic" :src="item.imgSrc" :alt="item.alt" />
+          <a :href="item.href" :target="item.target" :title="item.alt">
+            <img :width="item.width" :height="item.height" :class="item.class" :src="item.imgSrc" :alt="item.alt" />
           </a>
         </p>
         <p class="text">{{ item.text }}</p>
@@ -20,20 +20,24 @@ export default {
     return {
       applyData: [
          {
-          href: "https://cdn.jsdelivr.net/gh/itclanCode/blogimg@main/mIn-program-img/gh_a0b6a06d175a_258.6k0rqh7vefo0.jpg",
+          href: "javascript:;",
           imgSrc: "https://cdn.jsdelivr.net/gh/itclanCode/blogimg@main/mIn-program-img/gh_a0b6a06d175a_258.6k0rqh7vefo0.jpg",
           alt: "暖暖聊天恋爱话术小程序-高情商聊天对话,让撩妹,撩汉脱单更简单",
           text: "暖暖聊天恋爱话术",
           width: "85",
-          height: "85"
+          height: "85",
+          target: "_self",
+          class:"medium-zoom lazy img-pic"
         },
         {
-          href: "https://cdn.jsdelivr.net/gh/itclanCode/blogimg@main/mIn-program-img/轻记账小程序码.3unxzonk84a0.jpg",
+          href: "javascript:;",
           imgSrc: "https://cdn.jsdelivr.net/gh/itclanCode/blogimg@main/mIn-program-img/轻记账小程序码.3unxzonk84a0.jpg",
           alt: "轻记账-一款轻松微信记账小工具",
           text: "轻记账",
           width: "85",
-          height: "85"
+          height: "85",
+          target: "_self",
+          class:"medium-zoom lazy img-pic"
         },
         {
             href: "https://img.itclan.cn",
@@ -41,7 +45,9 @@ export default {
             alt: "客来图床-免费存储图片",
             text: "客来图床",
             width: "100",
-            height: "44"  
+            height: "44",
+            target: "_blank",
+            class:"lazy img-pic"  
         },
 
         {
@@ -50,7 +56,9 @@ export default {
             alt: "站集网-总有你不知道的好用的网站大全",
             text: "站集网",
             width: "100",
-            height: "44" 
+            height: "44",
+            target: "_blank",
+            class:"lazy img-pic" 
         },
 
       ]
